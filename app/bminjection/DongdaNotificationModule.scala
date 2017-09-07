17fbe6d4f2b6e4aad852482f5adcde849ae69f43
AlfredYang1986 <yangyuanpig@gmail.com>
2017-09-07 19:57:57 +0800

    注入模块 模块化分离

--- app/bminjection/DongdaNotificationModule.scala
+++ app/bminjection/DongdaNotificationModule.scala
@@ -2,7 +2,9 @@ package bminjection
 
 import javax.inject.Singleton
 
-import bminjection.notification.emxmpp.EMImpl
+//import bminjection.notification.emxmpp.EMImpl
+
+import com.pharbers.xmpp.em.EMImpl
 
 @Singleton
 class DongdaNotificationModule extends EMImpl
