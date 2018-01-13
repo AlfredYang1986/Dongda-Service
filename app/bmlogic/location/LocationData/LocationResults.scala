ca1b4ec4ea8b46879152c67d78e5c7b829abdd27
jeorch <jeorch@foxmail.com>
2018-01-13 13:51:40 +0800

    数据导出

--- app/bmlogic/location/LocationData/LocationResults.scala
+++ app/bmlogic/location/LocationData/LocationResults.scala
@@ -1,5 +1,6 @@
 package bmlogic.location.LocationData
 
+import bmlogic.common.mergestepresult.QueryImagesObjects
 import com.mongodb.casbah.Imports._
 import play.api.libs.json.JsValue
 import play.api.libs.json.Json.toJson
@@ -9,10 +10,15 @@ import play.api.libs.json.Json.toJson
   */
 trait LocationResults {
     implicit val slr : DBObject => Map[String, JsValue] = { obj =>
-        val pin = obj.getAs[MongoDBObject]("pin").get
-        val cor = pin.getAs[MongoDBList]("coordinates").get
-        val longitude = toJson(cor.head.asInstanceOf[Number].floatValue)
-        val latitude = toJson(cor.tail.head.asInstanceOf[Number].floatValue)
+
+        val (longitude, latitude) = obj.getAs[MongoDBObject]("pin").map { pin =>
+            val cor = pin.getAs[MongoDBList]("coordinates").get
+            val longitude = toJson(cor.head.asInstanceOf[Number].floatValue)
+            val latitude = toJson(cor.tail.head.asInstanceOf[Number].floatValue)
+            (longitude, latitude)
+        }.getOrElse((toJson(0.0), toJson(0.0)))
+
+//        val pin = obj.getAs[MongoDBObject]("pin").get
         Map(
             "address" -> toJson(obj.get("address").asInstanceOf[String]),
             "pin" -> toJson(
@@ -26,10 +32,15 @@ trait LocationResults {
 
     implicit val sldr : DBObject => Map[String, JsValue] = { obj =>
         val loc_img = obj.getAs[MongoDBList]("location_images").getOrElse(new MongoDBList())
-        val pin = obj.getAs[MongoDBObject]("pin").get
-        val cor = pin.getAs[MongoDBList]("coordinates").get
-        val longitude = toJson(cor.head.asInstanceOf[Number].floatValue)
-        val latitude = toJson(cor.tail.head.asInstanceOf[Number].floatValue)
+        val tmp = QueryImagesObjects(loc_img)
+
+        val (longitude, latitude) = obj.getAs[MongoDBObject]("pin").map { pin =>
+            val cor = pin.getAs[MongoDBList]("coordinates").get
+            val longitude = toJson(cor.head.asInstanceOf[Number].floatValue)
+            val latitude = toJson(cor.tail.head.asInstanceOf[Number].floatValue)
+            (longitude, latitude)
+        }.getOrElse((toJson(0.0), toJson(0.0)))
+
         Map(
             "location_id" -> toJson(obj.get("_id").asInstanceOf[ObjectId].toString),
             "address" -> toJson(obj.get("address").asInstanceOf[String]),
@@ -40,18 +51,22 @@ trait LocationResults {
                 )
             ),
             "friendliness" -> toJson(obj.getAs[MongoDBList]("friendliness").get.toList.asInstanceOf[List[String]]),
-            "location_images" -> toJson(if (loc_img.isEmpty) List.empty else loc_img.toList.map(x =>
-                Map("image" -> toJson(x.asInstanceOf[DBObject].getAs[String]("image").getOrElse("")),
-                    "tag" -> toJson(x.asInstanceOf[DBObject].getAs[String]("tag").getOrElse("")))
-            ))
+//            "location_images" -> toJson(if (loc_img.isEmpty) List.empty else loc_img.toList.map(x =>
+//                Map("image" -> toJson(x.asInstanceOf[DBObject].getAs[String]("image").getOrElse("")),
+//                    "tag" -> toJson(x.asInstanceOf[DBObject].getAs[String]("tag").getOrElse("")))
+            "location_images" -> tmp
         )
     }
 
     implicit val hsslr : DBObject => Map[String, JsValue] = { obj =>
-        val pin = obj.getAs[MongoDBObject]("pin").get
-        val cor = pin.getAs[MongoDBList]("coordinates").get
-        val longitude = toJson(cor.head.asInstanceOf[Number].floatValue)
-        val latitude = toJson(cor.tail.head.asInstanceOf[Number].floatValue)
+
+        val (longitude, latitude) = obj.getAs[MongoDBObject]("pin").map { pin =>
+            val cor = pin.getAs[MongoDBList]("coordinates").get
+            val longitude = toJson(cor.head.asInstanceOf[Number].floatValue)
+            val latitude = toJson(cor.tail.head.asInstanceOf[Number].floatValue)
+            (longitude, latitude)
+        }.getOrElse((toJson(0.0), toJson(0.0)))
+
         Map(
             "address" -> toJson(obj.get("address").asInstanceOf[String]),
             "pin" -> toJson(
